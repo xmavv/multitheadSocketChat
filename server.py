7d01efc8de539b166ba849a04414ce1079a4960f
xmavv <xxmatheoooo@gmail.com>
2025-05-01 16:36:34 +0200

    feat: add handling multiple clients

--- server.py
+++ server.py
@@ -3,15 +3,22 @@ import threading
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+clients = []
+
+def handle_client(socket):
+    while True:
+        data = socket.recv(1024)
+        socket.sendall(data)
+
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            conn.sendall(data)
+    while True:
+        socket, address = s.accept()
+        clients.append(socket)
+
+        print(f"client {address} connected")
+
+        client = threading.Thread(target=handle_client, args=(socket,))
+        client.start()

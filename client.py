46b8d05bca3c3e35124499176752d0e6ef8ce79d
xmavv <xxmatheoooo@gmail.com>
2025-05-01 16:15:09 +0200

    chore: initial basic echo server with one client

---
+++ client.py
@@ -0,0 +1,13 @@
+import socket
+
+HOST = "127.0.0.1"  # The server's hostname or IP address
+PORT = 65432  # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+
+    while True:
+        message = input()
+        s.sendall(message.encode())
+        data = s.recv(1024)
+        print(data)
